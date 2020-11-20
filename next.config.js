f53eff1aebdb4ee394e2df3816ef3500d8f3bcb9
Mrjen <improveselfchen@gmail.com>
2020-11-20 10:21:04 +0800

    compres

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withSass = require('@zeit/next-sass')
 module.exports = withSass({
   /* config options here */
+  compress: false,
   cssModules: true,
   cssLoaderOptions: {
     importLoaders: 1,
